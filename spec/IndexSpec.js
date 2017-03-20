367a2265638220295a5f9c1ca38495d7a925756b
romwaldtff <romwald@tff>
2017-03-20 10:08:56 +0100

    Add isNoGood test

---
+++ spec/IndexSpec.js
@@ -0,0 +1,11 @@
+describe("Index",() => {
+    describe("=> isNoGood", () => {
+        it("Should return false", next => {
+
+        });
+
+        it("Should return true", next => {
+
+        });
+    });
+});
