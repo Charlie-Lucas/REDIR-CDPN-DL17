eabfec5ca9d424b760c71b9d1724c762ea9e8969
Tetiana Savitska <savitska.tetiana@gmail.com>
2017-03-21 14:20:08 +0100

    add res.send and error

--- dev-back/src/Router/Router.js
+++ dev-back/src/Router/Router.js
@@ -7,10 +7,8 @@ const urlController = require('../Controller/UrlController');
 //user roots
 router.get('/', userController.indexAction);
 
-router.get('/register', userController.registerAction);
 router.post('/register', userController.signUpAction);
 
-router.get('/connect', userController.connectAction);
 router.post('/connect', userController.loginAction);
 
 router.get('/logout', userController.logoutAction);
