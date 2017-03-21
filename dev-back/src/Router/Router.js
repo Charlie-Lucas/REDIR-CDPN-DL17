701928233914098ea56a08c6a992fe89b9db76e7
Tetiana Savitska <savitska.tetiana@gmail.com>
2017-03-21 15:38:13 +0100

    modif for const

--- dev-back/src/Router/Router.js
+++ dev-back/src/Router/Router.js
@@ -5,13 +5,9 @@ const userController = require('../Controller/UserController');
 const urlController = require('../Controller/UrlController');
 
 //user roots
-// router.get('/', userController.indexAction);
-
-// router.post('/register', userController.signUpAction);
-
-// router.post('/connect', userController.loginAction);
-
-// router.get('/logout', userController.logoutAction);
+router.post('/register', userController.signUpAction);
+router.post('/connect', userController.loginAction);
+router.get('/logout', userController.logoutAction);
 
 //url roots
 router.get('/urls/:userId', urlController.getUrlsAction);
