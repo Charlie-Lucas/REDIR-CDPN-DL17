716894818c72704a23b3f2c67ac2e2f17e1c3448
Tetiana Savitska <savitska.tetiana@gmail.com>
2017-03-21 17:01:46 +0100

    C:/Users/Tetiana/AppData/Local/Programs/Git/register works

--- dev-back/src/Controller/UserController.js
+++ dev-back/src/Controller/UserController.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const app = express(); //one can type also require('express')();
+
 const UserModel = require('../Model/Entity/User.js');
 
 const validator = require('email-validator');
@@ -7,23 +10,35 @@ const hash = require('password-hash');
 const mongoose = require('mongoose');
 
 //validate that email is valid and verifies that email doesn't exist in database
-const emailValidator = (email) => {
-
+const emailValidator = (email, callback) => {
+    console.log('Email Validator enter in function');
     if (validator.validate(email)) {
         // check that email is unique
-        db.users.find({email: email}, (err, result) => {
-            if (err) {
+        mongoose.connect('mongodb://momo-bibi:imieimie@ds135820.mlab.com:35820/momo-bibi', (err) => {
+            if(err){
                 res.status(500).send({
                     error: err
                 });
             }
-            //if result is not empty list  !=[]
-            return !result.length ? true : {error: "Votre email existe déjà"};
+
+            UserModel.find({email: email}, (err, result) => {
+                console.log('Error' + err);
+                console.log('Result' + result);
+                if (err) {
+                    res.status(500).send({
+                        error: err
+                    });
+
+                }
+
+                //if result is not empty list  !=[]
+                callback(!result.length ? true : {error: "Votre email existe déjà"});
+            });
         });
     } else {
-        return {
+        callback({
             error: "Votre email n'est pas valide"
-        };
+        });
     }
 };
 
@@ -51,68 +66,101 @@ const passwordHash = (password) => {
 
 //on submit
 const signUpAction = (req, res) => {
+    console.log('ok');
     let post = req.body;
+    emailValidator(post.email, (emailStatus)=>{
+        console.log(emailStatus);
 
-    if (emailValidator(post.email) === true) {
+        if (emailStatus === true) {
 
-        if (passwordConfirm(post.password, post.passwordConfirmed)) {
+            let passwordStatus = passwordValidator(post.password);
+            if (passwordStatus === true){
+                if (passwordConfirm(post.password, post.passwordConfirmed)) {
 
-            mongoose.connect('mongodb://momo-bibi:imieimie@ds135820.mlab.com:35820/momo-bibi', () => {
+                    mongoose.connect('mongodb://momo-bibi:imieimie@ds135820.mlab.com:35820/momo-bibi', (err) => {
+                        if(err){
+                            res.status(500).send({
+                                error: err
+                            });
+                        }
 
-                let newUser = new UserModel({email: post.email, password: _self.passwordHash(post.password)});
+                        let newUser = new UserModel({email: post.email, password: passwordHash(post.password)});
 
-                newUser.save((err) => {
-                    if (err) {
-                        res.status(500).send({
-                            error: err
+                        newUser.save((err) => {
+                            if (err) {
+                                res.status(500).send({
+                                    error: err
+                                });
+                            }
+                            console.log('User saved to database !');
+                            res.status(200).send(newUser);
                         });
-                    }
-                    console.log('User saved to database !');
-                    res.status(200).send(newUser);
-                });
-            });
+                    });
 
-        }else {
-            console.log("Le mot de passe doit etre le meme");
-            res.status(400).send({
-                error: "Le mot de passe doit etre le meme"
-            });
+                }else {
+                    console.log("Le mot de passe doit etre le meme");
+                    res.status(400).send({
+                        error: "Le mot de passe doit etre le meme"
+                    });
+                }
+            } else {
+                console.log(passwordStatus.error);
+                res.status(400).send(passwordStatus);
+            }
+
+        } else {
+            console.log('EmailStauts ' + emailStatus.error);
+            res.status(400).send(emailStatus);
         }
+    });
 
-    } else {
-        console.log(emailValidator(post.email).error);
-        res.status(400).send(emailValidator(post.email));
-    }
 };
 
 //on submit
 const loginAction =  (req, res) => {
     let post = req.body;
-    db.user.find({
-        email: post.email
-    }, (err, user) => {
-        if (err) {
-            console.log(err);
+
+    mongoose.connect('mongodb://momo-bibi:imieimie@ds135820.mlab.com:35820/momo-bibi', (err) => {
+        if(err){
             res.status(500).send({
                 error: err
             });
+            return;
         }
-        if (user) {
-            if (hash.verify(post.password, user.password)) {
-                app.locals.user = user; //localStorage.setItem('user', user);
-                res.status(200).send(user);
-            } else {
-                console.log('Votre mot de passe est incorrect');
-                res.status(403).send({
-                    error: 'Votre mot de passe est incorrect'
+
+        UserModel.find({
+            email: post.email
+        }, (err, user) => {
+            if (err) {
+                console.log(err);
+                res.status(500).send({
+                    error: err
                 });
+                return;
             }
-        } else {
-            console.log('You have to register first');
-            res.status(403).send({
-                error: "Ce compte n'été pas trouvé"
-            });
-        }
+            console.log(user);
+            console.log(post.password);
+            console.log(user[0].password);
+            if ( user ) {
+                if (hash.verify(post.password, user[0].password)) {
+                    app.locals.user = user; //localStorage.setItem('user', user);
+                    res.status(200).send(user);
+                    return;
+                } else {
+                    console.log('Votre mot de passe est incorrect');
+                    res.status(403).send({
+                        error: 'Votre mot de passe est incorrect'
+                    });
+                    return;
+                }
+            } else {
+                console.log('You have to register first');
+                res.status(403).send({
+                    error: "Ce compte n'été pas trouvé"
+                });
+                return;
+            }
+        });
     });
 
 };
--- dev-back/src/Router/Router.js
+++ dev-back/src/Router/Router.js
@@ -7,17 +7,17 @@ const urlController = require('../Controller/UrlController');
 //user roots
 router.post('/register', userController.signUpAction);
 router.post('/connect', userController.loginAction);
-router.get('/logout', userController.logoutAction);
+//router.get('/logout', userController.logoutAction);
 
 //url roots
-router.get('/urls/:userId', urlController.getUrlsAction);
+//router.get('/urls/:userId', urlController.getUrlsAction);
 //router.post('/add-url', userController.addAction);
 
 //router.delete('/remove-url/:id(\\d+)', userController.removeAction);
 
 //url roots
-router.get('/urls', urlController.getUrlsAction);
-router.post('/urls', userController.addAction);
-router.delete('/urls/:id(\\d+)', userController.removeAction);
+//router.get('/urls', urlController.getUrlsAction);
+//router.post('/urls', userController.addAction);
+//router.delete('/urls/:id(\\d+)', userController.removeAction);
 
 module.exports = router;
