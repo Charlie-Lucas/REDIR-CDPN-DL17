c96f49155385f7a360ca1cfef55f11ca557db83e
romwaldtff <romwald@tff>
2017-03-21 18:48:57 +0100

    Update Routes to work with both Controllers

--- dev-back/src/Model/Entity/Url.js
+++ dev-back/src/Model/Entity/Url.js
@@ -6,9 +6,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let url = new Schema({
-    url: { type: String },
-    urlMinified: { type: String },
-    userId : {type: String }
+    url: { type: String , required: true},
+    urlMinified: { type: String , required: true},
+    userId : {type: String , required: true}
 });
 
 let UrlModel = mongoose.model('url', url);
--- dev-back/src/Router/Router.js
+++ dev-back/src/Router/Router.js
@@ -4,20 +4,16 @@ const router = express.Router();
 const userController = require('../Controller/UserController');
 const urlController = require('../Controller/UrlController');
 
-//user roots
+//user routes
 router.post('/register', userController.signUpAction);
 router.post('/connect', userController.loginAction);
 //router.get('/logout', userController.logoutAction);
 
-//url roots
-//router.get('/urls/:userId', urlController.getUrlsAction);
-//router.post('/add-url', userController.addAction);
-
-//router.delete('/remove-url/:id(\\d+)', userController.removeAction);
-
-//url roots
-//router.get('/urls', urlController.getUrlsAction);
-//router.post('/urls', userController.addAction);
-//router.delete('/urls/:id(\\d+)', userController.removeAction);
+//url routes
+router.get('/urls/:userId', urlController.getUrlsAction);
+router.post('/urls', urlController.addAction);
+router.get('/urls', urlController.getUrlsAction);
+router.delete('/urls/:urlId', urlController.removeAction);
+router.get('/:urlMin', urlController.getBigUrlByMinUrl);
 
 module.exports = router;
