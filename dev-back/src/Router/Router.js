2b1d726348bd90623e41edb6ec623a6f3958fb6b
Tetiana Savitska <savitska.tetiana@gmail.com>
2017-03-21 15:15:57 +0100

    StatusCode

--- dev-back/src/Router/Router.js
+++ dev-back/src/Router/Router.js
@@ -5,18 +5,13 @@ const userController = require('../Controller/UserController');
 const urlController = require('../Controller/UrlController');
 
 //user roots
-router.get('/', userController.indexAction);
-
 router.post('/register', userController.signUpAction);
-
 router.post('/connect', userController.loginAction);
-
 router.get('/logout', userController.logoutAction);
 
 //url roots
 router.get('/urls', urlController.getUrlsAction);
-router.post('/add-url', userController.addAction);
-
-router.delete('/remove-url/:id(\\d+)', userController.removeAction);
+router.post('/urls', userController.addAction);
+router.delete('/urls/:id(\\d+)', userController.removeAction);
 
 module.exports = router;
